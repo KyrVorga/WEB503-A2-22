8cb5400e11b930a03ac9d49b1d18a97cadfdc2f8
Rhylei Tremlett <Rhylei@aureate.dev>
2022-09-11 18:31:20 +1200

    completed mobile navbar

---
+++ src/home-page/home.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+	if (document.getElementById("dropdown-nav").style.display == "block") {
+		document.getElementById("dropdown-nav").style.display = "none";
+	} else {
+		document.getElementById("dropdown-nav").style.display = "block";
+	}
+}
