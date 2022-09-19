d6ccfbc32ea47d8c766e07de2e06a78a660b48dd
Rhylei Tremlett <Rhylei@aureate.dev>
2022-09-20 10:44:20 +1200

    finished template for mobile

--- src/template/home.js
+++ src/template/home.js
@@ -1,23 +1,23 @@
 function toggleMenu() {
-	if (document.getElementById("dropdown-nav").style.display == "block") {
-		document.getElementById("dropdown-nav").style.display = "none";
+	if (document.getElementById("navbar-items").style.margin == "0px") {
+		document.getElementById("navbar-items").style.margin = "-4.5em 0 0 0";
 	} else {
-		document.getElementById("dropdown-nav").style.display = "block";
+		document.getElementById("navbar-items").style.margin = "0px";
 	}
 }
 
 function toggleSidebar() {
 	const sidebar = document.getElementById("sidebar");
-	const sidebarButton = document.getElementById("sidebar-menu-button");
+	const sidebarButton = document.getElementById("menu-button-sidebar");
 	const buttonIcon = sidebarButton.firstElementChild;
 
-	if (document.getElementById("sidebar").style.height == "0px") {
-		sidebar.style.height = "40%";
-		sidebarButton.style.bottom = "40%";
-		buttonIcon.classList.replace("fa-chevron-up", "fa-chevron-down");
-	} else {
+	if (sidebar.style.height == "40%") {
 		sidebar.style.height = "0px";
 		sidebarButton.style.bottom = "0px";
 		buttonIcon.classList.replace("fa-chevron-down", "fa-chevron-up");
+	} else {
+		sidebar.style.height = "40%";
+		sidebarButton.style.bottom = "40%";
+		buttonIcon.classList.replace("fa-chevron-up", "fa-chevron-down");
 	}
 }
